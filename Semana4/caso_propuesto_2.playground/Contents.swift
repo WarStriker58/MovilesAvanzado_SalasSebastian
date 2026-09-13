c435a8d8b3b93019448e0115d7313811a1068c7f
WarStriker58 <150464131+WarStriker58@users.noreply.github.com>
2026-09-12 20:53:22 -0500

    base cliente caso propuesto 02 semana 4

--- Semana4/caso_propuesto_2.playground/Contents.swift
+++ Semana4/caso_propuesto_2.playground/Contents.swift
@@ -1,3 +1,26 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// base cliente
+class Cliente {
+    var codigo: String
+    var direccion: String
+    var fechaDeRegistro: String
+    var numeroCuenta: String
+    var montoMinimoApertura: Double
+    
+    init(codigo: String, direccion: String, fechaDeRegistro: String, numeroCuenta: String, montoMinimoApertura: Double) {
+        self.codigo = codigo
+        self.direccion = direccion
+        self.fechaDeRegistro = fechaDeRegistro
+        self.numeroCuenta = numeroCuenta
+        self.montoMinimoApertura = montoMinimoApertura
+    }
+    
+    func mostrarDatos() {
+        print("📄 Código: \(codigo)")
+        print("📍 Dirección: \(direccion)")
+        print("📅 Fecha de registro: \(fechaDeRegistro)")
+        print("🏦 Nº Cuenta: \(numeroCuenta)")
+        print("💰 Monto mínimo de apertura: S/ \(montoMinimoApertura)")
+    }
+}
