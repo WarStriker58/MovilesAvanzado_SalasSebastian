72009778155299fa49e6bde7aaf2eda42b553c02
WarStriker58 <150464131+WarStriker58@users.noreply.github.com>
2026-09-12 19:41:21 -0500

    Estructura base e ingreso de datos

--- Semana4/caso_propuesto_1.playground/Contents.swift
+++ Semana4/caso_propuesto_1.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// estructura base e ingreso de datos
+print("Ingrese el nombre del estudiante:")
+let estudiante = readLine() ?? ""
+
+print("Ingrese el DNI del estudiante:")
+let dni = readLine() ?? ""
+
+print("¿Es alumno de Tecsup? (Seleccione una opción numérica):")
+print("1. Sí")
+print("2. No")
+let opcionTecsup = readLine() ?? ""
+
+var textoAlumnoTecsup = ""
+var esAlumnoTecsup = false
+
+if opcionTecsup == "1" {
+    textoAlumnoTecsup = "Sí ✅"
+    esAlumnoTecsup = true
+} else {
+    textoAlumnoTecsup = "No"
+    esAlumnoTecsup = false
+}
