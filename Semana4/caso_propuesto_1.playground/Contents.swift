e3cee7a000ff1b76bb8201d7d1977e9b9e69f01a
WarStriker58 <150464131+WarStriker58@users.noreply.github.com>
2026-09-12 19:43:30 -0500

    Bucle interactivo para registrar multiples cursos
    Added interactive loop to register multiple courses with details.

--- Semana4/caso_propuesto_1.playground/Contents.swift
+++ Semana4/caso_propuesto_1.playground/Contents.swift
@@ -22,3 +22,40 @@ if opcionTecsup == "1" {
     textoAlumnoTecsup = "No"
     esAlumnoTecsup = false
 }
+
+// bucle interactivo para registrar multiples cursos
+print("\n¿Cuántos cursos distintos va a registrar?")
+let cantidadCursosDiferentesString = readLine() ?? ""
+let cantidadCursosDiferentes = Int(cantidadCursosDiferentesString) ?? 0
+
+var subtotal = 0.0
+var totalCursosComprados = 0
+var listaDeCursosImpresion = "" 
+
+var contador = 1
+
+while contador <= cantidadCursosDiferentes {
+    print("\n--- CURSO \(contador) ---")
+    
+    print("Nombre del curso:")
+    let nombreCurso = readLine() ?? ""
+    
+    print("Cantidad de vacantes para este curso:")
+    let cantString = readLine() ?? ""
+    let cant = Int(cantString) ?? 0
+    
+    print("Precio por unidad de este curso:")
+    let precioString = readLine() ?? ""
+    let precio = Double(precioString) ?? 0.0
+    
+    let totalPorCurso = Double(cant) * precio
+    
+    subtotal = subtotal + totalPorCurso
+    totalCursosComprados = totalCursosComprados + cant
+    
+    if cant > 0 {
+        listaDeCursosImpresion = listaDeCursosImpresion + nombreCurso + " x\(cant) - S/ \(totalPorCurso)\n"
+    }
+    
+    contador = contador + 1
+}
